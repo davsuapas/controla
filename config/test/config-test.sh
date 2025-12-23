faa79ad3bbe6a0df02104acb9daeb48483f7250c
davsuapas <dav.sua.pas@gmail.com>
2025-12-23 12:46:48 +0100

    Se crea la documentación en readme.md para realizar test de usuario

--- config/test/config-test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Este script genera un fichero configuración para ejecutar
-# controla-api en local
-
-./scripts/dist/config.sh ./config/test/config.json '' '/var/lib/mysql/mysql.sock' davsuapas controla 2 debug 5000 false false ''
